0b6cf9b986bbfafa952da58550028ad297d667f4
unknown <umairshabbir178@gmail.com>
2023-01-09 01:22:52 -0800

    sprint-3 new commit

--- Scripts/Index-page.js
+++ Scripts/Index-page.js
@@ -1,25 +1,37 @@
 const form = document.getElementById('bandsite-form');
 const commentlist = document.getElementById('comments-list');
 
-let comments = [
-    {
-        name: 'Connor Walton',
-        timestamp: '02/17/2021',
-        text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.'
-    },
-    {
-        name: 'Emilie Beach',
-        timestamp: '01/09/2021',
-        text: 'feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
-    },
-    {
-        name: 'Miles Acosta',
-        timestamp: '12/20/2020',
-        text:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    }
-];
+
+let url = "https://project-1-api.herokuapp.com"
+
+function getapikey() {
+    return axios.get(url + "/register")
+        .then((response) => {
+            //console.log(response.data.api_key)
+            let apikey = response.data.api_key
+            console.log(apikey)
+            console.log(typeof (apikey))
+            Promise.resolve(apikey)
+        })
+};
+
+function getcomments() {
+    getapikey().then(response => {
+        axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${response}`)
+        .then((response) => {
+            let comments = response.data
+            console.log(comments)
+            Promise.resolve(comments)
+            displayallcomments(comments)
+        }) 
+    })};
+
+getcomments();
+
+
+
 // function to create html markup for a comment
-function displaycomment(i){
+function displaycomment(comment) {
     //create li and append to ul
     const listElement = document.createElement("li");
     const ul = document.getElementById('comments-list')
@@ -47,58 +59,70 @@ function displaycomment(i){
     //create Name paragragraph, append to fourth div, and add text
     const p1 = document.createElement('p')
     p1.classList.add('comments__title')
-    p1.innerHTML = comments[i].name;
+    p1.innerHTML = comment.name;
     fourth_div.appendChild(p1);
     //create Date paragragraph, append to fourth div, and add date
     const p2 = document.createElement('p');
     p2.classList.add('comments__date');
-    p2.innerHTML = comments[i].timestamp;
-    fourth_div.appendChild(p2); 
+    p2.innerHTML = new Date(comment.timestamp).toLocaleDateString()
+    //p2.innerHTML = comment.timestamp;
+    fourth_div.appendChild(p2);
     //create comment paragrapha, append to third div, and add comment
     const p3 = document.createElement('p');
     p3.classList.add('comments__text')
     third_div.appendChild(p3);
-    p3.innerHTML = comments[i].text;
+    p3.innerHTML = comment.comment;
 }
 
-// function to create html mark up for all comments. 
-function displayallcomments(){
-    for (let i=0; i<comments.length; i++){
-        const y = comments[i];
-        displaycomment(i);
+//function to create html mark up for all comments. 
+function displayallcomments(comments) {
+    for (let i = 0; i < comments.length; i++) {
+        const comment = comments[i];
+        displaycomment(comment);
     }
 };
-displayallcomments();
 
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-//     const inputname = e.target.username.value;
-//     const inputcomment = e.target.usercomment.value;
-//     //addComment(inputname, new.Date().toLocaleDateString(), inputcomment);
-//     displaycomment(i);
-//     p1.innerHTML = inputname;
-//     p3.innerText = inputcomment;
-//     p2.innerHTML = new Date().toLocaleDateString()
-//     form.reset();
-// });
-
-
-
-
+function addcomment (inputname,inputcomment){
+    getapikey().then(response=>{
+        axios.post(`${url}/comments?api_key=${response}`, {
+            name: inputname,
+            comment: inputcomment
+        })
+        .then(response=>{
+            console.log(response.data)
+        })
+        .catch(e=>{
+            console.log(e)
+        })
+    })
+}
 
 
 
-
-
-
-// Adding name cooment in predefined html divs
-
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     const inputname = e.target.username.value;
     const inputcomment = e.target.usercomment.value;
+    addcomment(inputname,inputcomment)
+    getapikey().then(response=>{
+        axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${response}`)
+        .then(response=>{
+            let comments = response.data
+            Promise.resolve(comments)
+            let new_comments = comments.sort((a,b)=>{
+                return b.timestamp - a.timestamp
+            })
+            console.log(new_comments)
+            displaynewcomment(new_comments[0])
+        })
+    })
+    form.reset();
+});
 
-    //create li and append to ul
+
+// function to create html markup for the added comment
+function displaynewcomment(comment) {
+    //create li and prepend to ul
     const listElement = document.createElement("li");
     const ul = document.getElementById('comments-list')
     ul.prepend(listElement);
@@ -122,24 +146,25 @@ form.addEventListener('submit', function(e){
     const fourth_div = document.createElement('div');
     fourth_div.classList.add('comments__subcontainer3')
     third_div.appendChild(fourth_div);
-    //create paragragraph and append to fourth div
+    //create Name paragragraph, append to fourth div, and add text
     const p1 = document.createElement('p')
     p1.classList.add('comments__title')
-    const p2 = document.createElement('p')
-    p2.classList.add('comments__date')
-    fourth_div.appendChild(p1)
-    fourth_div.appendChild(p2) 
-    //create p3 and append to third div
-    const p3 = document.createElement('p')
+    p1.innerHTML = comment.name;
+    fourth_div.appendChild(p1);
+    //create Date paragragraph, append to fourth div, and add date
+    const p2 = document.createElement('p');
+    p2.classList.add('comments__date');
+    p2.innerHTML = new Date(comment.timestamp).toLocaleDateString()
+    //p2.innerHTML = comment.timestamp;
+    fourth_div.appendChild(p2);
+    //create comment paragrapha, append to third div, and add comment
+    const p3 = document.createElement('p');
     p3.classList.add('comments__text')
+    third_div.appendChild(p3);
+    p3.innerHTML = comment.comment;
+}
 
-    third_div.appendChild(p3)
 
-    p1.innerHTML = inputname;
-    p3.innerText = inputcomment;
-    p2.innerHTML = new Date().toLocaleDateString()
-    form.reset();
-});
 
 
 
